c82586fa1fbe1d5d86679bfd3d398326725a3324
JavadAlip <javadalipp911@gmail.com>
2024-04-01 12:26:56 +0530

    added responsive to all size devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',  
+        'md': '768px',  // Large Screens
+        'lg': '1024px', 
+        'xl': '1280px', 
+      },
+    },
   },
   plugins: [],
-}
-
+};
